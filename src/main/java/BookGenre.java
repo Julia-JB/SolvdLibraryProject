475d8ed5759b150382b33d20e571812a8cd8c9da
Julia J-B <4juliko@gmail.com>
2023-05-11 11:32:42 -0700

    AgeGroup Enum class added

--- src/main/java/BookGenre.java
+++ src/main/java/BookGenre.java
@@ -1,10 +1,28 @@
 public enum BookGenre {
-        FICTION,
-        NON_FICTION,
-        MYSTERY,
-        ROMANCE,
-        SCIENCE_FICTION,
-        CHILDRENS_PICTURE_BOOK,
-        CHILDRENS_CHAPTER_BOOK,
-        CHILDRENS_NOVEL;
+        FICTION("Fiction", "Books that are made up or imaginary"),
+        NON_FICTION("Non-Fiction", "Books that are based on facts or real events"),
+        MYSTERY("Mystery", "Books that involve the solving of a crime or puzzle"),
+        ROMANCE("Romance", "Books that focus on a romantic relationship between characters"),
+        SCIENCE_FICTION("Science Fiction", "Books that explore the possibilities of future " +
+                "science and technology"),
+        CHILDRENS_PICTURE_BOOK("Children's Picture Book", "Books that are illustrated with " +
+                "pictures and are designed for young children"),
+        CHILDRENS_CHAPTER_BOOK("Children's Chapter Book", "Books that are aimed at older children and are divided into chapters"),
+        CHILDRENS_NOVEL("Children's Novel", "Books that are longer and more complex than chapter books and are aimed at young adults");
+
+        private final String displayName;
+        private final String description;
+
+        BookGenre(String displayName, String description) {
+                this.displayName = displayName;
+                this.description = description;
+        }
+
+        public String getDisplayName() {
+                return displayName;
+        }
+
+        public String getDescription() {
+                return description;
+        }
 }
