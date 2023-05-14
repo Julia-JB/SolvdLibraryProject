cfe2a66e1258a0aca3775248cac47f7fdec10830
Julia J-B <4juliko@gmail.com>
2023-05-13 22:29:20 -0700

    Loops refactoring

--- src/main/java/ChildrensBook.java
+++ src/main/java/ChildrensBook.java
@@ -1,13 +1,9 @@
 public class ChildrensBook extends Book {
-
     // Fields
     private AgeGroup ageGroup;
 
     // Constructor
-    public ChildrensBook(int id, ItemType itemType, String title, String author, double price,
-                         boolean available,
-                        int numberOfItemsAvailable, BookGenre genre, String ISBN, int pageCount,
-                         AgeGroup ageGroup) {
+    public ChildrensBook(int id, ItemType itemType, String title, String author, double price, boolean available, int numberOfItemsAvailable, BookGenre genre, String ISBN, int pageCount, AgeGroup ageGroup) {
         super(id, itemType, title, author, price, available, numberOfItemsAvailable, genre, ISBN, pageCount);
         this.ageGroup = ageGroup;
     }
@@ -21,11 +17,11 @@ public class ChildrensBook extends Book {
         this.ageGroup = ageGroup;
     }
 
-   public void checkAgeGroup(int userAge) {
+    public void checkAgeGroup(int userAge) {
         if (this.getAgeGroup().isAgeRange(userAge)) {
             logger.info("This book is suitable for the user's age.");
         } else {
             logger.info("This book may not suitable for the user's age.");
         }
-   }
+    }
 }
